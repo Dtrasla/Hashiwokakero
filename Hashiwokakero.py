4d27df9571140047d9d0bf154323f0bb203a770f
Daniel Traslavina <danstervan1@gmail.com>
2023-03-28 22:14:32 -0500

    Improved printing

--- Hashiwokakero.py
+++ Hashiwokakero.py
@@ -1,5 +1,18 @@
 def printMap(islands):
+    marker = "   "
+    for j in range(len(islands[0])):
+        if j%2 == 0:
+            marker += "[" + str(j//2) + "]"
+        else:
+            marker += " "
+    print(marker)
+
+
     for i in range(len(islands)):
+        if i%2 == 0:
+            print(( "[" + str(i//2) + "]"), end=" ")
+        else:
+            print("   ", end=" ")
         for j in range(len(islands[i])):
             print(islands[i][j], end=" ")
         print()
@@ -12,15 +25,23 @@ def loadIslands(filename):
         islands = [[0 for x in range(width*2)] for y in range(height*2)] 
         for i in range(height):
             line = file.readline().strip()
-            for j in range(width):                    
-                islands[i*2][j*2] = int(line[j])
-                islands[i*2][j*2+1] = "~"
+            for j in range(width):
+                val = line[j]
+                if val == "0":
+                    islands[i*2][j*2] = "≈"
+                else:
+                    islands[i*2][j*2] = int(val)
+                islands[i*2][j*2+1] = "≈"
         for i in range(height):
             for j in range(width*2):
-                islands[i*2+1][j] = "~"
+                islands[i*2+1][j] = "≈"
     return islands
 
 
+
+
+
+
 #main
-islands = loadIslands('islas.in')
+islands = loadIslands('islands.in')
 printMap(islands)
