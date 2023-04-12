8f51a25e114052912eb2a7d01b3dc5159208a117
Daniel Traslavina <danstervan1@gmail.com>
2023-04-11 21:30:46 -0500

    Bridge adding

--- Hashiwokakero.py
+++ Hashiwokakero.py
@@ -1,3 +1,5 @@
+import hashiwokakero_classes as hc
+
 def printMap(islands):
     marker = "   "
     for j in range(len(islands[0])):
@@ -17,7 +19,7 @@ def printMap(islands):
             print(islands[i][j], end=" ")
         print()
 
-def loadIslands(filename):
+def loadIslands(filename, il):
     with open(filename, 'r') as file:
         line = file.readline().strip()
         width, height = map(int, line.split(","))
@@ -30,7 +32,8 @@ def loadIslands(filename):
                 if val == "0":
                     islands[i*2][j*2] = "≈"
                 else:
-                    islands[i*2][j*2] = int(val)
+                    islands[i*2][j*2] = val
+                    il.append(hc.Island(j, i, int(val)))
                 islands[i*2][j*2+1] = "≈"
         for i in range(height):
             for j in range(width*2):
@@ -38,10 +41,108 @@ def loadIslands(filename):
     return islands
 
 
+def drawBridges(bridge, islands):
+    drawing = ""
+    if bridge.island1.x == bridge.island2.x:
+        if bridge.num_bridges == 1:
+            drawing = "|"
+        elif bridge.num_bridges == 2:
+            drawing = "║"
+        else :
+            drawing = "≈"
+        if bridge.island1.y < bridge.island2.y:
+            for i in range((bridge.island1.y*2)+1, bridge.island2.y*2):
+                islands[i][bridge.island1.x*2] = drawing
+        else:
+            for i in range((bridge.island2.y*2)+1, bridge.island1.y*2):
+                islands[i][bridge.island1.x*2] = drawing
+
+    elif bridge.island1.y == bridge.island2.y:
+        if bridge.num_bridges == 1:
+            drawing = "-"
+        elif bridge.num_bridges == 2:
+            drawing = "="
+        else :
+            drawing = "≈"
+        if bridge.island1.x < bridge.island2.x:
+            for i in range(bridge.island1.x + 1, bridge.island2.x * 2):
+                islands[bridge.island1.y*2][i] = drawing
+        else:
+            for i in range(bridge.island2.x + 1, bridge.island1.x * 2):
+                islands[bridge.island1.y*2][i] = drawing
+    
+
+def checkCoordinates(bridge1, bridge2):
+    if bridge1.island1.x == bridge2.island1.x and bridge1.island1.y == bridge2.island1.y:
+        if bridge1.island2.x == bridge2.island2.x and bridge1.island2.y == bridge2.island2.y:
+            return True
+
+    elif bridge1.island1.x == bridge2.island2.x and bridge1.island1.y == bridge2.island2.y:
+        if bridge1.island2.x == bridge2.island1.x and bridge1.island2.y == bridge2.island1.y:
+            return True
+
+    else:
+        return False
 
 
+def checkValidBridge(bridge, islands):
+    val = True
+    if (bridge.island1.x == bridge.island2.x) or (bridge.island1.y == bridge.island2.y):
+        for i in range (bridge.island1.x, bridge.island2.x, 2):
+            if islands[bridge.island1.y][i] != "≈":
+                val = False
+                break
+
+
+    return val
 
 
 #main
-islands = loadIslands('islands.in')
+il = []
+islands = loadIslands('islands.in', il)
+
+bridges = []
 printMap(islands)
+
+finish = False
+
+while not finish:
+    print("1. Add bridge")
+    print("2. Remove bridge")
+    print("3. Exit")
+    option = int(input("Select an option: "))
+    if option == 1:
+        print("Add bridge")
+        x = int(input("x Island 1: "))
+        y = int(input("y Island 1: "))
+
+        if islands[y*2][x*2].isdigit():
+            x1 = int(input("x Island 2: "))
+            y1 = int(input("y Island 2: "))
+            if islands[y1*2][x1*2].isdigit():
+                bri = hc.Bridge(hc.Coordinate(x, y), hc.Coordinate(x1, y1))
+                found = False
+                for br in bridges:
+                    #TODO revisar valido en br encontrado con valor 0
+                    if checkCoordinates(bri, br):
+                        found = True
+                        br.add_bridge()
+                        drawBridges(br, islands)
+                        printMap(islands)
+                        break
+                if not found:
+                    bridges.append(bri)
+
+                    drawBridges(bri, islands)
+                    printMap(islands)
+        else:
+            print("Bridge")
+
+
+    elif option == 2:
+        print("Remove bridge")
+    elif option == 3:
+        finish = True
+    else:
+        print("Invalid option")
+        
---
+++ hashiwokakero_classes.py
@@ -0,0 +1,35 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Bridge:
+    def __init__(self, coordinate1, coordinate2):
+        self.island1 = Coordinate(coordinate1.x, coordinate1.y)
+        self.island2 = Coordinate(coordinate2.x,coordinate2.y)
+        self.num_bridges = 1  # comienza con 1 puente
+    
+    def add_bridge(self):
+        if self.num_bridges < 2:
+            self.num_bridges += 1
+    
+    def remove_bridge(self):
+        if self.num_bridges > 0:
+            self.num_bridges -= 1
+    
+    def __repr__(self):
+        return f"Bridge between {self.island1} and {self.island2} ({self.num_bridges} bridge{'s' if self.num_bridges > 1 else ''})"
+
+
+class Island:
+    def __init__(self, x, y, num_bridges):
+        self.x = x
+        self.y = y
+        self.num_bridges = num_bridges
+    
+    def __str__(self):
+        return f"Island ({self.x}, {self.y}) with {self.num_bridges} bridges"
